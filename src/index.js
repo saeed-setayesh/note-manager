b080948233c0bf618944629cd54d2b1f05ff77b9
Saeed Setayesh <iamsaeedsetayesh@gmail.com>
2020-09-23 20:04:05 +0330

    finalize project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages/Note-List';
+import App from './pages/notes';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/notes.js
+++ src/pages/notes.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import produce from 'immer';
+import "./notes.scss"
 
 export default () => {
-  const initialData = [{ text: 'Loading Notes ... ' }];
-  const [data, setData] = useState(initialData);
+  const [data, setData] = useState([]);
 
   const handleClick = () => {
     const text = document.querySelector('#noteinput').value.trim();
@@ -20,6 +20,15 @@ export default () => {
       setData(nextState);
     }
   };
+
+  const handleChange = (e, index) => {
+    let items = [...data];
+    let item = { ...items[index] };
+    item.text = e.target.value
+    items[index] = item;
+    setData(items)
+    localStorage.setItem('data', JSON.stringify(items));
+  }
 
   const handleRemove = (index) => {
     const newData = data.filter((note) => {
@@ -42,12 +51,19 @@ export default () => {
 
   return (
     <>
-      <input id="noteinput" style={{ width: '80%' }} type="text" placeholder="Enter a new note" />
-      <button onClick={() => handleClick()}>Add note</button>
-      {Object.keys(data).map((note, index) => <div key={index}>
-        <input onChange={(e) => { setData({ ...data, [index]: { ...data[index], text: e.target.value } }) }} type="text" value={data[note].text} />
-        <div onClick={(e) => handleRemove(data[index])}>Remove</div>
-      </div>)}
+      <div className="container">
+        <div className="forms">
+          <div className='notes'>
+            <input id="noteinput" type="text" placeholder="Enter a new note" />
+            <div className="addItem" onClick={() => handleClick()}>Add note</div>
+          </div>
+          <h3 className="title-notes">Notes</h3>
+          {Object.keys(data).map((note, index) => <div className='notes' key={index}>
+            <input onChange={(e) => handleChange(e, index)} type="text" value={data[note].text} />
+            <div className="remove" onClick={() => handleRemove(data[index])}>Remove</div>
+          </div>)}
+        </div>
+      </div>
     </>
   );
 };
